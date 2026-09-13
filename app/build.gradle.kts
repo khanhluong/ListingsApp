ca059aabf1f3c8c8a01cac1c0006a8d07f532176
baokhanh.luong <baokhanh2710cntt@gmail.com>
2026-09-13 15:19:02 +1200

    chore: add colors and icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.trademe.listingsapp"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
